f234d248117e970b18ba0f53332f90c7dacfc99c
zabulyaka <zabulyaka@gmail.com>
2025-08-27 15:10:57 +0500

    First pages added: index, about

---
+++ hexlet_django_blog/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+def index(request):
+    return render(
+        request,
+        'index.html',
+        context={
+            'who': 'World',
+        },
+    )
+
+def about(request):
+    return render(request, 'about.html')
